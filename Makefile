fbf5cc03a3e0b770ebf70236cc9759bcecc7b2aa
Luca Barbosa <lucabento.luca@gmail.com>
2025-11-04 16:29:04 -0300

    initializing pipex

---
+++ Makefile
@@ -0,0 +1,37 @@
+NAME		=	pipex
+CC			=	cc
+CFLAGS	=	-Wall -Wextra -Werror
+LIBFT		=	libft/libft.a
+INC		=	-I ./src -I ./libft
+OBJ		=	$(patsubst src/%.c, obj/%.o, $(SRC))
+SRC		=	src/pipex.c \
+
+all:		$(LIBFT) obj $(NAME)
+
+$(NAME):	$(OBJ)
+			$(CC) $(CFLAGS) -o $@ $^ $(LIBFT)
+			@echo "\033[32mPipex Compiled!\033[0m"
+
+obj:
+			@mkdir -p obj
+
+obj/%.o:	src/%.c
+			$(CC) $(CFLAGS) $(INC) -o $@ -c $<
+
+$(LIBFT):		
+			@make -s -C libft
+			@echo "Libft compiled!"
+
+clean:
+			@make -s $@ -C libft
+			@rm -rf $(OBJ) $(LIBFT) obj
+			@echo "\033[1;35mObjects deleted.\033[0m"
+
+fclean:	clean
+			@make -s fclean -C libft
+			@rm -rf $(NAME)
+			@echo "\033[1;36mPipex deleted.\033[0m"
+
+re:			fclean all
+
+.PHONY:		all bonus clean fclean re
---
+++ src/pipex.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbento <lbento@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/04 16:27:23 by lbento            #+#    #+#             */
+/*   Updated: 2025/11/04 16:28:50 by lbento           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <sys/uio.h>
+# include <sys/wait.h>
+# include <sys/types.h>
+# include "../libft/libft.h"
+
+#endif
